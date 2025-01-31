59a102939e8f1a9e457445ba3f1842c66281f7cd
Vartika <vartikajohari@cdac.in>
2025-01-31 14:45:51 +0530

    add command for creating dll file of the particular project

--- CommonService/obj/Debug/net8.0/CommonService.AssemblyInfo.cs
+++ CommonService/obj/Debug/net8.0/CommonService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e229ce92f805672d2c28a863f4eb15c3a57b4ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15519d4e5fd985e1eb353cc7a3c20c011686c7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
